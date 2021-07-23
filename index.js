c4426ab5e7fb89561c6c416286d24b66febd8318
Philip Tronser <tronser@web.de>
2021-07-23 10:02:09 +0200

    controllers updated 1

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ import recipeRouter from './routes/recipeRouter';
 
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.use()
-
-app
-  .route('/')
-  .get((req, res) => res.send('We retrieve a resource '))
-  .post((req, res) => res.send('We create a resource '))
-  .put((req, res) => res.send('We update a resource '))
-  .delete((req, res) => res.send('We delete a resource '));
-
+app.use('/recipes, recipeRouter');
 app.listen(port, () =>   console.log(`Server listening at http://localhost:${port}`));
